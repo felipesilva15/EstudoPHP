8c675f6fff6c54e426b9c3672c4c290317b96a60
Felipe.Silva <felipe.allware@gmail.com>
2022-08-01 17:28:44 -0300

    Corrigindo nome da pasta de variáveis

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php 
+
+$nome = "Felipe";
+$sobrenome = "Silva";
+$nomecompleto = $nome." ".$sobrenome;
+
+echo "O nome é  $nome e o sobrenome é $sobrenome. Juntos eles formam o nome completo $nomecompleto";
+echo "<br>";
+echo "<br>";
+
+var_dump($nome);
+echo "<br>";
+
+var_dump($sobrenome);
+echo "<br>";
+
+var_dump($nomecompleto);
+echo "<br>";
+
+?>
