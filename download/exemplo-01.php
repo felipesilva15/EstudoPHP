55c98506853dde0b80bf9116c630ec98809c9aaa
Felipe.Silva <felipe.allware@gmail.com>
2022-08-21 04:20:20 -0300

    Adicionando comentários no código

--- download/exemplo-01.php
+++ download/exemplo-01.php
@@ -1,18 +1,26 @@
 <?php
 
-$fileLink = "https://avatars.githubusercontent.com/u/58152964?s=48&v=4";
+// Define a URL do arquivo que será baixado
+$fileLink = "https://www.gstatic.com/youtube/img/promos/growth/9ed4da82df05e8d7cc3dc14d4848a5a07d0b109d0bab9d83db02c19e5d436070_122x56.webp";
 
+// Pega o conteúdo do arquivo
 $content = file_get_contents($fileLink);
 
+// Pega as informações do arquivo
 $parse = parse_url($fileLink);
+
+// Pega o nome do arquivo
 $basename = basename($parse["path"]);
 
+// Cria um arquivo com o nome original
 $file = fopen($basename, "w+");
 
+// Escreve o conteúdo baixado no arquivo criado
 fwrite($file, $content);
 
 fclose($file);
 
+// Exibe o arquivo baixado na tela
 echo "<img src=\"$basename\" >";
 
 ?>
