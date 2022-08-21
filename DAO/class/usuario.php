364071222ed1f31e55bfa76bbaf8e8f483fd43d0
Felipe.Silva <felipe.allware@gmail.com>
2022-08-20 23:49:41 -0300

    Criando a métodos para consulta de todos usuários, consulta por login e autenticação de usuário

--- DAO/class/usuario.php
+++ DAO/class/usuario.php
@@ -36,9 +36,9 @@ class Usuario {
     }
 
     public function loadById($id){
-        $sql = new Sql;
+        $sql = new Sql();
 
-        $data = $sql->select("SELECT * FROM tb_usuarios WHERE :ID", array(
+        $data = $sql->select("SELECT * FROM tb_usuarios WHERE pkId = :ID", array(
             ":ID"=>$id
         ));
 
@@ -49,6 +49,40 @@ class Usuario {
             $this->setDsLogin($row["dsLogin"]);
             $this->setDsSenha($row["dsSenha"]);
             $this->setDtCadastro(new DateTime($row["dtCadastro"]));
+        }
+    }
+
+    public static function getList():array{
+        $sql = new Sql();
+
+        return ($sql->select("SELECT * FROM tb_usuarios ORDER BY dsLogin"));
+    }
+
+    public static function searchByLogin($login):array{
+        $sql = new Sql();
+
+        return ($sql->select("SELECT * FROM tb_usuarios WHERE dsLogin LIKE :LOGIN ORDER BY dsLogin", array(
+            ":LOGIN"=>"%$login%"
+        )));
+    }
+
+    public function login($login, $senha){
+        $sql = new Sql();
+
+        $data = $sql->select("SELECT * FROM tb_usuarios WHERE dsLogin = :LOGIN AND dsSenha = :SENHA", array(
+            ":LOGIN"=>$login,
+            ":SENHA"=>$senha
+        ));
+
+        if (isset($data[0])){
+            $row = $data[0];
+
+            $this->setPkId($row["pkId"]);
+            $this->setDsLogin($row["dsLogin"]);
+            $this->setDsSenha($row["dsSenha"]);
+            $this->setDtCadastro(new DateTime($row["dtCadastro"]));
+        }else{
+            throw new Exception("Login e/ou senha inválidos", 1);
         }
     }
 
--- DAO/index.php
+++ DAO/index.php
@@ -4,6 +4,28 @@ require_once("config.php");
 
 $usuario = new Usuario;
 
-$usuario->loadById(4);
+// Carrega um usuário pelo ID
+// $usuario->loadById(4);
+
+// echo $usuario;
+
+
+
+// Carrega uma lista de usuários
+// $data = Usuario::getList();
+
+// echo json_encode($data);
+
+
+
+// Carrega uma lista de usuários filtrando pelo login
+// $data = Usuario::searchByLogin("root");
+
+// echo json_encode($data);
+
+
+    
+// Carrega um usuário utilizando um login e senha
+$usuario->login("felipe.silva", "123");
 
 echo $usuario;
