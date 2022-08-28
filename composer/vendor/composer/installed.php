4b8a830f4879b936fd69fbc8636d84ad5aeaabb4
Felipe.Silva <felipe.allware@gmail.com>
2022-08-28 04:10:17 -0300

    Estudo sobre o composer para controlar dependências do projeto

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'estudo/composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '599b298ae6e9685a8674c8f72df4440ecbe8ad11',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'estudo/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '599b298ae6e9685a8674c8f72df4440ecbe8ad11',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
